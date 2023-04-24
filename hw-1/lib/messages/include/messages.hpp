8ee1321284d2c3aba886578756d2e3915e8b7f04
Vilinvil <ivn15072002@gmail.com>
2023-04-24 16:44:37 +0300

    fix definition of messages

--- hw-1/lib/messages/include/messages.hpp
+++ hw-1/lib/messages/include/messages.hpp
@@ -1,8 +1,9 @@
 #pragma once
+#include <string>
 
-const char *mes_bad_argument =
+const std::string mes_bad_argument =
     "Bad request. Write --help or -h to see how utility work";
 
-const char *mes_help = "Command should be used in format: \
+const std::string mes_help = "Command should be used in format: \
 hw1 --path_artist|-a [your path to artist file] --path_type|-t [your path to artist_type] \
 --query|-q [your query for searh] right only in this order";
