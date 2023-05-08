c0cea3e529ec2344db5bb159e87f60f5c9f92d1f
Vilinvil <ivn15072002@gmail.com>
2023-05-08 11:29:32 +0300

    decomposed func PiplineHandler

--- hw-2/src/main.cpp
+++ hw-2/src/main.cpp
@@ -7,45 +7,51 @@
 #include "operations.hpp"
 #include "pipe_parser.hpp"
 
-// PiplineHandler() can throw std::runtime_error
-static void PiplineHandler(std::string &pipe, std::ostream &out) {
-    try {
-        IOperationUP head;
-        IOperation *tail;
+namespace {
+void piplineHandler(std::string &pipe, std::ostream &out) {
+    IOperationUP head;
+    IOperation *tail;
 
-        std::vector<Command> command_v = ParsePipe(pipe);
+    std::vector<Command> command_v = ParsePipe(pipe);
 
-        bool flagFirst = true;
-        for (auto it = command_v.begin(); it != command_v.end(); it++) {
-            IOperation *tmp;
-            if (it->operation_ == "echo") {
-                tmp = new EchoOperation(it->arg_, out);
-            } else if (it->operation_ == "cat") {
-                tmp = new CatOperation(it->arg_, out);
-            } else if (it->operation_ == "wc") {
-                tmp = new WCOperation(it->arg_, out);
-            } else {
-                throw std::runtime_error(
-                    " For help use ./hw2 --help. Not supported command: " +
-                    it->str());
-            }
-
-            if (flagFirst) {
-                head = std::unique_ptr<IOperation>(tmp);
-                tail = head.get();
-                flagFirst = false;
-                continue;
-            }
-
-            tail->SetNextOperation(tmp);
-            tail = tmp;
+    bool flagFirst = true;
+    for (auto it = command_v.begin(); it != command_v.end(); it++) {
+        IOperation *tmp;
+        if (it->operation_ == "echo") {
+            tmp = new EchoOperation(it->arg_, out);
+        } else if (it->operation_ == "cat") {
+            tmp = new CatOperation(it->arg_, out);
+        } else if (it->operation_ == "wc") {
+            tmp = new WCOperation(it->arg_, out);
+        } else {
+            throw std::runtime_error(
+                " For help use ./hw2 --help. Not supported command: " +
+                it->str());
         }
-        head->HandleEndOfInput();
+
+        if (flagFirst) {
+            head = std::unique_ptr<IOperation>(tmp);
+            tail = head.get();
+            flagFirst = false;
+            continue;
+        }
+
+        tail->SetNextOperation(tmp);
+        tail = tmp;
+    }
+    head->HandleEndOfInput();
+}
+
+// PiplineHandler() can throw std::runtime_error
+void PiplineHandler(std::string &pipe, std::ostream &out) {
+    try {
+        piplineHandler(pipe, out);
     } catch (std::exception &e) {
         throw std::runtime_error("in PiplineHandler: exeption: " +
                                  static_cast<std::string>(e.what()));
     }
 }
+}   // namespace
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
